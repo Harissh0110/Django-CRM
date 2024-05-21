eadf021764cdbee053fcb285adb7f6a28c22d8ca
Harissh0110 <Harissh0110@gmail.com>
2024-05-21 18:19:08 +0530

    urls edit

--- website/urls.py
+++ website/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_login, name='login'),
-    path('home/', views.home, name='home'),
+    path('login/', views.user_login, name='login'),
+    path('', views.home, name='home'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('record/<int:pk>', views.customer_record, name='record'),
